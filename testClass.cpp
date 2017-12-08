98e548dcd8ee3d398f62b286e4496bbc9de70050
uozkl <zli203@uottawa.ca>
2017-12-07 23:09:43 -0500

    v1.0.2

--- testClass.cpp
+++ testClass.cpp
@@ -14,5 +14,40 @@ using namespace std;
 int main()
 {
     cout << "This is the test class for all the classes" << endl;
+    int index;
+    bool test = true;
+    while (test)
+    {
+        cout << "You can test folling classes:" << endl;
+        cout << "1.QwintoPlayer" << endl
+             << "2.QwintoRow" << endl
+             << "3.QwintoScoreSheet" << endl
+             << "4.QwintoPlayer" << endl
+             << "5.QwixxPlayer" << endl
+             << "6.QwixxRow" << endl
+             << "7.QwixxScoreSheet" << endl
+             << "8.RollOfDice" << endl;
+        cout << "Input index to start test:" << endl;
+        cin >> index;
+        switch (index)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        }
+    }
     return 0;
 }
