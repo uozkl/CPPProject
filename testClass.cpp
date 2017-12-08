1a0f155ee4c23da60a317e4cfa1f3a83ac8626de
uozkl <zli203@uottawa.ca>
2017-12-07 23:47:24 -0500

    v1.0.4

--- testClass.cpp
+++ testClass.cpp
@@ -1,4 +1,4 @@
-#ifdef TEST
+// #ifdef TEST
 #include "QwintoPlayer.h"
 #include "QwintoRow.h"
 #include "QwintoScoreSheet.h"
@@ -20,17 +20,27 @@ int main()
     char a;
     while (test)
     {
+        string playerName = "test";
+        RollOfDice rd;
+        RollOfDice rodR, rodG;
+        Colour red = Colour::RED;
+        Colour green = Colour::GREEN;
+        Dice *reddice = new Dice(red);
+        Dice *greendice = new Dice(green);
+        QwixxRow<vector<int>, Colour::RED> redRow;
+        QwixxRow<list<int>, Colour::GREEN> greenRow;
         test = false;
-        cout << "You can test folling classes:" << endl;
-        cout << "1.QwintoPlayer" << endl
+
+        cout << "You can test folling classes:" << endl
+             << "1.QwintoPlayer" << endl
              << "2.QwintoRow" << endl
              << "3.QwintoScoreSheet" << endl
              << "4.QwintoPlayer" << endl
              << "5.QwixxPlayer" << endl
              << "6.QwixxRow" << endl
              << "7.QwixxScoreSheet" << endl
-             << "8.RollOfDice" << endl;
-        cout << "Input index to start test:" << endl;
+             << "8.RollOfDice" << endl
+             << "Input index to start test:" << endl;
         cin >> index;
         switch (index)
         {
@@ -43,8 +53,32 @@ int main()
         case 4:
             break;
         case 5:
+            cout << "Testing QwixxPlayer.cpp" << endl;
+            cout << "Build a player named test and set him active" << endl;
+            QwintoScoreSheet *qtss = new QwintoScoreSheet(playerName);
+            QwintoPlayer *player = new QwintoPlayer(*qtss);
+            (*player).active = true;
+            cout << "Build roll of dice, add dice and roll" << endl;
+            (*player).inputBeforeRoll(rd);
+            rd.roll();
+            cout << "Show score sheet, let player make choice" << endl;
+            (*player).inputBeforeRoll(rd);
+            cout << endl
+                 << "End of test" << endl
+                 << endl;
             break;
         case 6:
+            cout << "Testing QwixxRow.cpp" << endl;
+            cout << "Build a vector row red and list row green" << endl;
+            rodR.add(*reddice);
+            rodG.add(*greendice);
+            rodR.roll();
+            rodG.roll();
+            cout << "Print two rows" << endl;
+            cout << "Red: " << redRow << endl;
+            cout << "Green: " << greenRow << endl;
+            cout << "Add roll of dice";
+
             break;
         case 7:
             break;
@@ -58,4 +92,4 @@ int main()
     }
     return 0;
 }
-#endif
+// #endif
