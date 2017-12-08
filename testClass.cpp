5c7900dec3d2151b3912fa96cdeedabbf3b827e7
uozkl <zli203@uottawa.ca>
2017-12-07 23:01:26 -0500

    v1.0.1

---
+++ testClass.cpp
@@ -0,0 +1,18 @@
+#include "QwintoPlayer.h"
+#include "QwintoRow.h"
+#include "QwintoScoreSheet.h"
+#include "QwintoPlayer.h"
+#include "QwixxPlayer.h"
+#include "QwixxRow.h"
+#include "QwixxScoreSheet.h"
+#include "RollOfDice.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+using namespace std;
+int main()
+{
+    cout << "This is the test class for all the classes" << endl;
+    return 0;
+}
