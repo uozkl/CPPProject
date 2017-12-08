05e41d21600ec888676787e5b67c90e9f0a10df7
uozkl <33238951+uozkl@users.noreply.github.com>
2017-12-07 21:56:48 -0500

    Release V1.0

--- RandomDice.h
+++ RandomDice.h
@@ -11,12 +11,9 @@
 
 struct RandomDice {
 
-	// static std::random_device randDev;
 	static std::mt19937 generator;
 	static std::uniform_int_distribution<unsigned> distribution;
-	//RandomDice();
 	static int getDice();
-
 };
 
 #endif /* RANDOMDICE_H */
